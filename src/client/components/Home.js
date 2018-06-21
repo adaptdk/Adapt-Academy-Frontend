13ca20328321a4416dc2a269f0a8eb30e9ab3bb3
Viktor Nareiko <viktor.nareiko@adapt.dk>
2018-06-22 00:19:46 +0300

    Fixed invalid nesting of DOM.

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -14,31 +14,29 @@ const Home = () => (
         <p>
           You have successfully started our react boilerplate which will let you:
         </p>
-        <p>
-          <ul>
-            <li>
-              become familiar with ES6 syntax, callbacks, promises and other weird words
-            </li>
-            <li>
-              become familiar with Node.js, npm and yarn
-            </li>
-            <li>
-              to learn React.js
-            </li>
-            <li>
-              to learn how to use existing React.js libraries for solving almost all tasks
-            </li>
-            <li>
-              to learn how integrate libraries that are no compatible with npm or React.js
-            </li>
-            <li>
-              to learn about application deployment
-            </li>
-            <li>
-              and more ...
-            </li>
-          </ul>
-        </p>
+        <ul>
+          <li>
+            become familiar with ES6 syntax, callbacks, promises and other weird words
+          </li>
+          <li>
+            become familiar with Node.js, npm and yarn
+          </li>
+          <li>
+            to learn React.js
+          </li>
+          <li>
+            to learn how to use existing React.js libraries for solving almost all tasks
+          </li>
+          <li>
+            to learn how integrate libraries that are no compatible with npm or React.js
+          </li>
+          <li>
+            to learn about application deployment
+          </li>
+          <li>
+            and more ...
+          </li>
+        </ul>
       </div>
     </Paper>
   </Columns>
