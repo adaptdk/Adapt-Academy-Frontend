56e51f9e3eaf4fda911e0893a4014374583bd134
Viktor Nareiko <viktor.nareiko@adapt.dk>
2018-07-04 18:10:56 +0300

    * Menu item is not shown when no displayName property; * Simplified logic for menu showing; * Small router renderer refactoring;

--- src/client/components/base/Nav.js
+++ src/client/components/base/Nav.js
@@ -1,46 +1,47 @@
+// eslint-disable-global location
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import {
+  arrayOf,
+  shape,
+  string,
+} from 'prop-types';
 import { isString } from 'lodash';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+const propTypes = {
+  routes: arrayOf(shape({
+    path: string,
+    displayName: string,
+  })).isRequired,
+};
 
 const Nav = ({ routes }) => (
   <Fragment>
-    {
-      /*
-        We have two default routes. One is a root and another one is a home.
-        Display navigation when we have more than default routes.
-      */
-      routes.length > 2 && (
-        <nav className="nav">
-          <ul className="nav__items">
-            {
-              routes.map(({ path, displayName }, idx) => {
-                  const activeMenuClassName = location.pathname.includes(path) ? 'nav__item--active' : ''; // eslint-disable-line no-restricted-globals
-
-                  return (
-                    isString(path) && path !== '/' &&
-                    <li
-                      key={ idx }
-                      className={ `nav__item ${activeMenuClassName}` }
-                    >
-                      <Link to={ path }>{ displayName }</Link>
-                    </li>
-                  );
-                },
-              )
-            }
-          </ul>
-        </nav>
-      )
-    }
+    <nav className="nav">
+      <ul className="nav__items">
+        {
+          routes.map(({ path, displayName }, idx) => (
+              isString(path) && !!displayName &&
+                <li
+                  key={ idx }
+                  className={ classNames(
+                      'nav__item',
+                      {
+                        'nav__item--active': location.pathname.includes(path), // eslint-disable-line no-restricted-globals
+                      },
+                    ) }
+                >
+                  <Link to={ path }>{ displayName }</Link>
+                </li>
+            ),
+          )
+        }
+      </ul>
+    </nav>
   </Fragment>
 );
 
-Nav.propTypes = {
-  routes: PropTypes.arrayOf(PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    displayName: PropTypes.string.isRequired,
-  })).isRequired,
-};
+Nav.propTypes = propTypes;
 
 export default Nav;
--- src/client/routes.js
+++ src/client/routes.js
@@ -6,13 +6,11 @@ import HomeContainer from './containers/HomeContainer';
 const routes = [
   {
     path: '/',
-    displayName: 'Root',
     exact: true,
     Component: () => <RootRedirector to="/home" />,
   },
   {
     path: '/home',
-    displayName: 'Welcome to Adapt',
     exact: true,
     Component: HomeContainer,
   },
