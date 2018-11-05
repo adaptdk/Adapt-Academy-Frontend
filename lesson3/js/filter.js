657ebb6df4dc5c92fa9d07d4f727543aa6bc5b60
Eimantas <tygasz@gmail.com>
2018-11-05 17:35:53 +0200

    changes

--- lesson3/js/filter.js
+++ lesson3/js/filter.js
@@ -1,18 +1,20 @@
+var fetch = require('node-fetch');
+
+
 (function getData() {
   fetch('https://raw.githubusercontent.com/adaptdk/Adapt-Academy-Frontend/master/lesson3/users.json')
   .then(function(response){ return response.json()})
   .then(function(data) {
     const parsedData = JSON.parse(JSON.stringify(data));
-    salaryFilter(parsedData);
+    return salaryFilter(parsedData);
   })
 
   .catch(error => console.error(error))
   })();
 
 function salaryFilter(data){
-  const user = data.filter(function(item){
+  return data.filter(function(item){
     if (item.salary > 1000)
     return item;
   })
-  console.log(user);
 }
