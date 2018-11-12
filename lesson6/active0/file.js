b4bf6943885e517a607dcaef5f16cb6b9f581ec1
Gytis Alčiauskas <alciauskas@gmail.com>
2018-11-12 17:17:14 +0200

    Added more stuff.

--- lesson6/active0/file.js
+++ lesson6/active0/file.js
@@ -1,20 +1,23 @@
 const myFunction = () => {
 	let foo = "my string"
 
-	const bar = true ? foo : true;
+	const bar = foo ? foo : false;
 
 	console.log(bar);
 
 	for (var i = 0;    i < 8; i++) {
 		for (var k = 0; k < 8; k++) {
 			for (var k = 0; k < 8; k++) {
-
+				bar = 'nex string' + k + i;
 			}
 		}
 	}
 
+	if(bar == true){
+		bar = 'new string which bis very long and have nothingh in it so to make it fold or smth idk'
+	}
 
-	bar = 'nex string';
+
 
 	return bar;
 
