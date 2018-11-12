0713d2028417625b77a5255d1684058394d3e153
Gytis Alčiauskas <alciauskas@gmail.com>
2018-11-12 16:58:22 +0200

    Init lesson 6.

---
+++ lesson6/active0/file.js
@@ -0,0 +1,23 @@
+const myFunction = () => {
+	let foo = "my string"
+
+	const bar = true ? foo : true;
+
+	console.log(bar);
+
+	for (var i = 0;    i < 8; i++) {
+		for (var k = 0; k < 8; k++) {
+			for (var k = 0; k < 8; k++) {
+
+			}
+		}
+	}
+
+
+	bar = 'nex string';
+
+	return bar;
+
+}
+
+myFunction();
