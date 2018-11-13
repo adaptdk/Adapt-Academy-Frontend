9ce539d7ede32012ab4d986b1c32aed422e43cea
Sarunas Zube <sarunas.zube@gmail.com>
2018-11-13 10:40:45 +0200

    First excersise

---
+++ lesson4/js/country.js
@@ -0,0 +1,12 @@
+const array = [
+    {"name": "Afghanistan", "code": "AF"},
+    {"name": "Åland Islands", "code": "AX"},
+    {"name": "Albania", "code": "AL"},
+    {"name": "Algeria", "code": "DZ"}
+];
+
+const getCountryByCode = (code) =>{ let output =  array.filter((x) =>{ return (code == x.code) });
+    console.log(output[0].name);
+}
+
+getCountryByCode('AX');
