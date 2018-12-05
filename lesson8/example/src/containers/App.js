bd3bd280f3c34b19fee36f5376d7afd6ae1b9fd9
Gytis Alčiauskas <alciauskas@gmail.com>
2018-12-05 12:33:06 +0200

    Adding lesson 8 example.

---
+++ lesson8/example/src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// Assets.
+import logo from '../assets/svg/logo.svg';
+import '../assets/styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1>Lesson 8 Example</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
