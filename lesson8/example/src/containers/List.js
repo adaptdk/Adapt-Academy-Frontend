2fab4ff665ffe01cc37ace2c11fc81b9484dbbbc
Gytis Alčiauskas <alciauskas@gmail.com>
2018-12-05 12:24:52 +0200

    Finished exmaple

--- lesson8/example/src/containers/List.js
+++ lesson8/example/src/containers/List.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 // Components.
 import ListHeaderItem from '../components/list/ListHeaderItem';
+import ListImage from '../components/list/ListImage';
+import Listitems from '../components/list/ListItems';
 
 // Constants.
 import { LIST_ITEMS } from '../contants/list'
@@ -10,12 +12,20 @@ class List extends React.Component {
   render() {
     return (
       <article>
+
         {LIST_ITEMS.map(({ title }, key) => (
           <ListHeaderItem
             key={key}
             title={title}
           />
         ))}
+
+        <ListImage/>
+
+        <Listitems
+          items={LIST_ITEMS[0].data}
+        />
+
       </article>
     );
   }
