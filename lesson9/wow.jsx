4e877aef602df07763b31c3955c12f171bf9761c
Mantas Būdvytas <wow@gmail.com>
2019-12-16 23:03:22 +0200

    :)

---
+++ lesson9/wow.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class wow extends Component {
+    render() {
+        return (
+            <div>
+                {
+                    (() => {
+                        if (flag && flag2 && flag3) {
+                            if (flag4) {
+                                return <p>Blah</p>
+                            } else if (flag5) {
+                                return <p>Meh</p>
+                            } else {
+                                return <p>Herp</p>
+                            } 
+                        } else {
+                            return <p>Derp</p>
+                        }
+                    })()
+                }
+            </div>
+        );
+    }
+}
+
+export default wow;
